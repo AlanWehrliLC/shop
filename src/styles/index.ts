3f3884e47f123478e5494b8b651bd01968f97055
AlanWehrliLC <wehrlialan1@gmail.com>
2022-10-17 15:05:40 -0300

    Configuring the stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import {createStitches} from "@stitches/react"
+
+export const {
+    config,
+    styled,
+    css,
+    globalCss,
+    keyframes,
+    getCssText,
+    theme,
+    createTheme,
+  } = createStitches({
+    theme: {
+      colors: {
+        shop: '#91e7d9',
+      }
+    }
+  })
