798444f7f12374a650fb2323cd18876f372f2b8e
AlanWehrliLC <wehrlialan1@gmail.com>
2022-10-18 04:27:27 -0300

    Creating Sales Item Page and Checkout with Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,25 +1,101 @@
-import {useRouter} from "next/router"
+import axios from "axios"
+import { GetStaticPaths, GetStaticProps } from "next"
+import Image from "next/image"
+import { useState } from "react"
+import Stripe from "stripe"
+import { stripe } from "../../lib/stripe"
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
-export default function Product(){
-    const {query} = useRouter()
+interface ProductProps {
+    product:{
+        id: string
+        name: string
+        imageUrl: string
+        url: string
+        price: string
+        description: string
+        defaultPriceId: string
+    }
+}
+
+export default function Product({ product }:ProductProps){
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+    async function handleBuyProduct(){
+        try {
+            setIsCreatingCheckoutSession(true)
+
+            const response = await axios.post("/api/checkout",{
+                priceId: product.defaultPriceId
+            })
+
+            const {checkoutUrl} = response.data
+
+            window.location.href = checkoutUrl
+        } catch (error) {
+            setIsCreatingCheckoutSession(false)
+
+            alert("Failed to redirect to checkout!")
+        }
+    }
 
     return (
         <ProductContainer>
             <ImageContainer>
-
+                <Image src={ product.imageUrl } width={520} height={480} alt="" />
             </ImageContainer>
 
             <ProductDetails>
-                <h1>T-Shirt</h1>
-                <span>$29.90</span>
+                <h1>{ product.name }</h1>
+                <span>{ product.price }</span>
 
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur laborum architecto et voluptate vel. Id, reiciendis. Voluptatem ratione aspernatur facilis, esse unde ullam delectus alias praesentium laboriosam a eum numquam?</p>
+                <p>{ product.description }</p>
 
-                <button>
-                    Buy now
+                <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+                    Buy now!
                 </button>
             </ProductDetails>
         </ProductContainer>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async ()=>{
+    return {
+        paths: [
+            {
+                params: {
+                    id: ""
+                }
+            }
+        ],
+        fallback: "blocking"
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({params}) => {
+    const productId = params.id
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ["default_price"]
+    })
+
+    const price = product.default_price as Stripe.Price
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                url: product.url,
+                price: new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD"
+                }).format(price.unit_amount / 100),
+                description: product.description,
+                defaultPriceId: price.id
+            }
+        },
+        revalidate: 60 * 60 * 1, // 1 hour
+    }
 }
