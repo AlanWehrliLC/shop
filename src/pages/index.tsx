af9eed1eb6c6884ecc5b66a4ef08616eb1183917
AlanWehrliLC <wehrlialan1@gmail.com>
2022-10-18 02:12:42 -0300

    Searching Stripe products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,54 +8,69 @@ import tShirt2 from "../assets/t-shirt/2.png"
 import tShirt3 from "../assets/t-shirt/3.png"
 
 import "keen-slider/keen-slider.min.css"
+import { stripe } from "../lib/stripe"
+import { GetServerSideProps } from "next"
+import Stripe from "stripe"
 
-function Home() {
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    url: string
+    price: number
+  }[]
+}
+
+export default function Home({products}: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
-      perView: 3,
+      perView: 2,
       spacing: 48
     }
   })
 
     return (
       <HomeContainer ref={sliderRef} className="keen-slider">
-        <Product className="keen-slider__slide">
-          <Image src={tShirt1}  width={520} height={480} alt=""/>
+        {products.map(product => {
+          return (
+            <Product key={product.id} className="keen-slider__slide">
+              <Image src={product.imageUrl}  width={520} height={480} alt=""/>
 
-          <footer>
-            <strong>Camiseta Beyond the Limits</strong>
-            <span>$59.90</span>
-          </footer>
-        </Product>
-
-        <Product className="keen-slider__slide">
-          <Image src={tShirt2}  width={520} height={480} alt=""/>
-
-          <footer>
-            <strong>Camiseta Beyond the Limits</strong>
-            <span>$59.90</span>
-          </footer>
-        </Product>
-
-        <Product className="keen-slider__slide">
-          <Image src={tShirt3}  width={520} height={480} alt=""/>
-
-          <footer>
-            <strong>Camiseta Beyond the Limits</strong>
-            <span>$59.90</span>
-          </footer>
-        </Product>
-
-        <Product className="keen-slider__slide">
-          <Image src={tShirt1}  width={520} height={480} alt=""/>
-
-          <footer>
-            <strong>Camiseta Beyond the Limits</strong>
-            <span>$59.90</span>
-          </footer>
-        </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>${product.price}</span>
+              </footer>
+            </Product>
+          )
+        })}
       </HomeContainer>
     )
   }
+
+export const getServerSideProps: GetServerSideProps = async ()=>{
+  const response = await stripe.products.list({
+    expand: ["data.default_price"]
+  })
+
   
-export default Home
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      url: product.url,
+      price: price.unit_amount / 100
+    }
+  })
+
+  console.log(response.data)
+
+  return {
+    props: {
+      products
+    }
+  }
+}
